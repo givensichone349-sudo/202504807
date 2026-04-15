a1ee3116ff36b3c27ef1604a91d446319b595d1b
givensichone349-sudo <givensichone349@gmail.com>
2026-04-15 14:14:08 +0200

    Implement factorial function in Python
    Fixes a typo in the variable name and corrects indentation for the return statement.

---
+++ factorial.py
@@ -0,0 +1,8 @@
+# This the code for the given assingment about creating a python function
+def factorial(n):
+  rersult = 1
+  for i in range(1, n + 1):
+    result = result * i
+    return result
+num = int(input("Enter a number : ")
+          print("Factorial is:", factorial(num))
